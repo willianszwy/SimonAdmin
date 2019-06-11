551239dd403df625e181bef1fdb1bebe2c43cf96
willians oliveira <willianszwy@gmail.com>
2019-06-10 21:26:42 -0400

    Updates

---
+++ precache-manifest.c21bd8683daeb43ab4df8c0c19e8ca4d.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e219b8e6c8dd2b325fb0c4b49559215",
+    "url": "/SimonAdmin/index.html"
+  },
+  {
+    "revision": "4ed87e403832c9f66dae",
+    "url": "/SimonAdmin/static/css/2.14786dd8.chunk.css"
+  },
+  {
+    "revision": "4a2669d966bbb4a651db",
+    "url": "/SimonAdmin/static/css/main.f8281da9.chunk.css"
+  },
+  {
+    "revision": "4ed87e403832c9f66dae",
+    "url": "/SimonAdmin/static/js/2.93b0d8b8.chunk.js"
+  },
+  {
+    "revision": "4a2669d966bbb4a651db",
+    "url": "/SimonAdmin/static/js/main.bcd435f9.chunk.js"
+  },
+  {
+    "revision": "fa0c7faa7c17c48e4eaa",
+    "url": "/SimonAdmin/static/js/runtime~main.4642fae6.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/SimonAdmin/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/SimonAdmin/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/SimonAdmin/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/SimonAdmin/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/SimonAdmin/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/SimonAdmin/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/SimonAdmin/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/SimonAdmin/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/SimonAdmin/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/SimonAdmin/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/SimonAdmin/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/SimonAdmin/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/SimonAdmin/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/SimonAdmin/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/SimonAdmin/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/SimonAdmin/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
